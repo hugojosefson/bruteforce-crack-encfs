ebda6c7e870ead0d3f34ff02cd85d5acfe784b9c
Hugo Josefson <hugo@josefson.org>
2019-01-05 22:08:39 +0100

    Update script.

--- bruteforce-encfs.sh
+++ bruteforce-encfs.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # initially from http://bredsaal.dk/cracking-encfs-made-easier
+# via https://gist.github.com/Finkregh/4864521a29a3fa204dc9
 
-# usage: crackencfs.sh /path/to/encrypted/folder /path/to/mountpoint /path/to/wordlist
+# usage: bruteforce-encfs.sh /path/to/encrypted/folder /path/to/mountpoint /path/to/wordlist
 counter=1
 
 while [ true ]; do
-   echo "$(head -n $counter $3 | tail -n 1)"
    echo "$(head -n $counter $3 | tail -n 1)" | encfs $1 $2 --stdinpass 
     if [ $? -eq 0 ]; then
         echo Key recovered - the password is: 
