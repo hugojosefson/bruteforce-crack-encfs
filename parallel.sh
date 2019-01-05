d197ecb972139be63afe36ec6991dd7e21d369ea
Hugo Josefson <hugo@josefson.org>
2019-01-05 21:52:01 +0100

    Protect temp files.

--- parallel.sh
+++ parallel.sh
@@ -4,6 +4,8 @@ split -l 30 $3 $3-split-
 # run bruteforce in parallel:
 for PWLIST in $(ls $3-split-*)
   do mkdir -p /tmp/${PWLIST}
+  touch /tmp/${PWLIST}-crack.log
+  chmod -R go= /tmp/${PWLIST}*
   nice $(dirname $0)/bruteforce-encfs.sh \
     $1 /tmp/${PWLIST} ${PWLIST} \
    >> /tmp/${PWLIST}-crack.log 2>&1 &
