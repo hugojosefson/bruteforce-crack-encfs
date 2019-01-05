20fda7a6f5fe5bc3a577d7b2e2162919cf82b788
Hugo Josefson <hugo@josefson.org>
2019-01-05 22:14:11 +0100

    Sane comment order.

--- bruteforce-encfs.sh
+++ bruteforce-encfs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# initially from http://bredsaal.dk/cracking-encfs-made-easier
 # via https://gist.github.com/Finkregh/4864521a29a3fa204dc9
+# initially from http://bredsaal.dk/cracking-encfs-made-easier
 
 # usage: bruteforce-encfs.sh /path/to/encrypted/folder /path/to/mountpoint /path/to/wordlist
 counter=1
